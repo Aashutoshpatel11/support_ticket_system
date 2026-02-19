865cbba4067e038075db2983e5cffb7e65582211
Aashutosh Patel <aashutoshpatel1110@gmail.com>
2026-02-19 22:16:32 +0530

    add. configure environment variables and update llm_service to load dotenv

--- Backend/tickets/llm_service.py
+++ Backend/tickets/llm_service.py
@@ -5,6 +5,9 @@ from typing import Literal
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
 
 logger = logging.getLogger(__name__)
 
